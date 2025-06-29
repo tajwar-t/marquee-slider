d55c995adcef322d7b7b8f271972649b4b158df0
tajwar-t <tajwar@bitbirds.com>
2025-06-29 17:05:03 +0600

    updated

--- js/marquee-slider.js
+++ js/marquee-slider.js
@@ -7,50 +7,111 @@ jQuery(document).ready(function($) {
     $('.marquee-slider').each(function(index) {
         var $slider = $(this);
         var sliderId = $slider.data('slider-id') || 'unknown';
-        var direction = $slider.data('direction') || 'left';
         var $inner = $slider.find('.marquee-inner');
-        var $images = $slider.find('img');
-        var imageCount = $images.length;
+        var speed = parseInt($slider.data('speed')) || 50;
+        var direction = $slider.data('direction') || 'left';
+        var totalWidth = 0;
+        var singleSetWidth = 0;
+        var wrapperCount = 0;
 
-        console.log('Marquee Slider ' + index + ' (ID: ' + sliderId + '): Found ' + imageCount + ' images, initializing with direction ' + direction);
+        console.log('Marquee Slider ' + index + ' (ID: ' + sliderId + '): Initializing with speed ' + speed + ' and direction ' + direction);
 
-        if (imageCount === 0) {
+        // Calculate widths
+        function calculateWidths() {
+            singleSetWidth = 0;
+            totalWidth = 0;
+            var $wrappers = $inner.find('.marquee-image-wrapper');
+            wrapperCount = $wrappers.length;
+            var imagesPerSet = Math.ceil(wrapperCount / 4); // Assume at least 4x duplication
+            $wrappers.each(function(i) {
+                var wrapperWidth = $(this).outerWidth(true);
+                totalWidth += wrapperWidth;
+                if (i < imagesPerSet) {
+                    singleSetWidth += wrapperWidth;
+                }
+            });
+            console.log('Marquee Slider ' + index + ' (ID: ' + sliderId + '): Single set width: ' + singleSetWidth + ', Total width: ' + totalWidth);
+            return singleSetWidth > 0 && totalWidth > 0;
+        }
+
+        // Animation state
+        var pos = direction === 'left' ? 0 : -singleSetWidth;
+
+        function animate(timestamp) {
+            if (!lastTime) lastTime = timestamp;
+            var deltaTime = (timestamp - lastTime) / 1000;
+            lastTime = timestamp;
+
+            if (direction === 'left') {
+                pos -= speed * deltaTime;
+                if (pos <= -singleSetWidth) {
+                    pos += singleSetWidth;
+                    // Move first wrapper to end for seamless looping
+                    $inner.append($inner.find('.marquee-image-wrapper').first());
+                }
+            } else {
+                pos += speed * deltaTime;
+                if (pos >= 0) {
+                    pos -= singleSetWidth;
+                    // Move last wrapper to start
+                    $inner.prepend($inner.find('.marquee-image-wrapper').last());
+                }
+            }
+
+            $inner.css('transform', 'translateX(' + pos + 'px)');
+
+            requestAnimationFrame(animate);
+        }
+
+        // Wait for images to load
+        var images = $inner.find('img');
+        var loadedImages = 0;
+        var lastTime = null;
+
+        if (images.length === 0) {
             console.warn('Marquee Slider ' + index + ' (ID: ' + sliderId + '): No images found in slider.');
             return;
         }
 
-        // Wait for images to load
-        var loadedImages = 0;
-        $images.each(function() {
+        images.each(function(i) {
             if (this.complete && this.naturalWidth !== 0) {
                 loadedImages++;
             } else {
                 $(this).on('load', function() {
                     loadedImages++;
-                    if (loadedImages === imageCount) {
-                        initializeSlider($slider, sliderId, index);
+                    if (loadedImages === images.length) {
+                        console.log('Marquee Slider ' + index + ' (ID: ' + sliderId + '): All images loaded, starting animation.');
+                        if (calculateWidths()) {
+                            $inner.css('width', 'auto');
+                            pos = direction === 'left' ? 0 : -singleSetWidth;
+                            requestAnimationFrame(animate);
+                        } else {
+                            console.warn('Marquee Slider ' + index + ' (ID: ' + sliderId + '): No valid image set width detected.');
+                        }
                     }
                 }).on('error', function() {
                     console.warn('Marquee Slider ' + index + ' (ID: ' + sliderId + '): Image failed to load: ' + $(this).attr('src'));
                     loadedImages++;
-                    if (loadedImages === imageCount) {
-                        initializeSlider($slider, sliderId, index);
+                    if (loadedImages === images.length) {
+                        if (calculateWidths()) {
+                            $inner.css('width', 'auto');
+                            pos = direction === 'left' ? 0 : -singleSetWidth;
+                            requestAnimationFrame(animate);
+                        }
                     }
                 });
             }
         });
 
-        if (loadedImages === imageCount) {
-            initializeSlider($slider, sliderId, index);
+        if (loadedImages === images.length) {
+            console.log('Marquee Slider ' + index + ' (ID: ' + sliderId + '): Images already loaded, starting animation.');
+            if (calculateWidths()) {
+                $inner.css('width', 'auto');
+                pos = direction === 'left' ? 0 : -singleSetWidth;
+                requestAnimationFrame(animate);
+            } else {
+                console.warn('Marquee Slider ' + index + ' (ID: ' + sliderId + '): No valid image set width detected.');
+            }
         }
     });
-
-    function initializeSlider($slider, sliderId, index) {
-        var $inner = $slider.find('.marquee-inner');
-        $inner.css({
-            'animation-play-state': 'running',
-            'display': 'inline-flex' // Ensure display is set
-        });
-        console.log('Marquee Slider ' + index + ' (ID: ' + sliderId + '): CSS animation initialized.');
-    }
 });
